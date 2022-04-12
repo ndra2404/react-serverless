f4334de4c187c2f2d729f11739f9f34dfa0d7fdd
Indra PC <ndra2404@gmail.com>
2022-04-12 09:54:09 +0700

    serverless

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React okay
+          Learn React okay indra bisa serverless
         </a>
       </header>
     </div>
